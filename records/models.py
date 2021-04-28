cec8fa2b89e84e913099a5e1855a8c192296778a
Safdar Ahmed <safdarahmed@MacAir.local>
2021-04-28 15:47:29 +0530

    added address field

--- records/models.py
+++ records/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Record(models.Model):
+    address = models.CharField(max_length=100, primary_key=True)
     harvest = models.CharField(max_length=50)
     seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='seller_set')
     buyer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='buyer_set')
